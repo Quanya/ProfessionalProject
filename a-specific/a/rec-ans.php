4bbc0f398dba4ac155bad77b4d3e8ec9b994ce7b
307482373 <dailin_307482373@qq.com>
2013-06-09 20:59:38 +0800

    Signed-off-by: 307482373 <dailin_307482373@qq.com>

---
+++ a-specific/a/rec-ans.php
@@ -0,0 +1,21 @@
+<?php
+//echo @$_POST["mySelect2"];
+$people=$_POST["mySelect2"];
+//echo $people;
+//echo "<br />";
+
+//echo @$_POST["radioSet"];
+$brand=$_POST["radioSet"];
+//echo $brand;
+//echo "<br />";
+
+//echo @$_POST["textinput"];
+$min=$_POST["textinput"];
+//echo $min;
+//echo "<br />";
+
+//echo @$_POST["textinput2"];
+$max=$_POST["textinput2"];
+//echo $max;
+
+?>
--- a-specific/a/recommend.php
+++ a-specific/a/recommend.php
@@ -1,9 +1,18 @@
 <html style="" class=" js no-touch cssanimations csstransitions"><head>
 <meta http-equiv="Content-Type" content="text/html; charset=gb2312">
 <title>Blog Design</title>
-<link rel="stylesheet" type="text/css" href="style1-keyword.css">
+<link rel="stylesheet" type="text/css" href="style1-recommend.css">
+<!--body -->
+<script type="text/javascript" src="js-com/jquery.js"></script>
+<script type="text/javascript" src="js-com/nicejforms.js"></script>
+<style type="text/css" media="screen">
+@import url(css-com/niceforms-green.css);
+</style>
 
-
+<!--img show-->
+<link rel="stylesheet" type="text/css" href="css-com/component.css">
+<script src="js-com/modernizr.custom.js"></script>
+<!--end-->
 </head>
 
 <body>
@@ -29,18 +38,100 @@
 				<div><span class="date">Mar 12th</span><span class="categories">in: Design, Themes</span></div>
 				<div class="description">
 				
-				  <div class="main clearfix">
-				   
-				    
+				<!--img show-->
 				
-			 
+						<div class="container">	
 			
 			
-			   
-			   
-			   
-			     </div>
-				  				
+			
+			<div class="main">
+				<div id="fc-slideshow" class="fc-slideshow">
+					<ul class="fc-slides">
+						<li style="display: none;"><img src="images/1.jpg"><h3>给力</h3></li>
+						<li style="display: none;"><img src="images/2.jpg"><h3>推荐</h3></li>
+						<li style="display: list-item;"><img src="images/3.jpg"><h3>不错</h3></li>
+						<li style="display: none;"><img src="images/4.jpg"><h3>还行</h3></li>
+						<li style="display: none;"><img src="images/5.jpg"><h3>可以</h3></li>
+						<li style="display: none;"><img src="images/6.jpg"><h3>差不多</h3></li>
+						<li style="display: none;"><img src="images/7.jpg"><h3>不好</h3></li>
+						<li style="display: none;"><img src="images/8.jpg"><h3>Cold</h3></li>
+					</ul>
+				<nav><div class="fc-left"><span></span><span></span><span></span><i class="icon-arrow-left"></i></div><div class="fc-right"><span></span><span></span><span></span><i class="icon-arrow-right"></i></div></nav><div class="fc-flip" style="-webkit-transition: none; -webkit-transform: none; display: none;"><div class="fc-front"><div><img src="images/2.jpg"><h3>Cold</h3></div><div class="fc-overlay-light" style="-webkit-transition: opacity 538.4615384615385ms; opacity: 1;"></div></div><div class="fc-back" style="-webkit-transform: rotate3d(0, 1, 0, 180deg);"><div><img src="images/3.jpg"><h3>Light</h3></div><div class="fc-overlay-dark" style="opacity: 0; -webkit-transition: opacity 538.4615384615385ms;"></div></div></div></div>
+			</div>
+		</div><!-- /container -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="js-com/jquery.flipshow.js"></script>
+		<script>
+			$( function() {
+				
+				$( '#fc-slideshow' ).flipshow();
+
+			} );
+		</script>
+				
+				
+				
+				
+				<!--end-->
+				
+				
+				
+				
+				 <!--body-->
+<div id="container">
+  <h1 style="margin-bottom:30px; color:#99cc33;">Commend</h1>
+  <form action="rec-ans.php" method="POST" class="niceform">  
+    <select size="1" id="mySelect2" name="mySelect2" class="width_160">
+      <option value="1">婴儿</option>
+      <option value="2">儿童</option>
+      <option selected="selected" value="3">学生</option>
+      <option value="4">成年人</option>
+      <option value="5">中老年人</option>
+	  <option value="6">女人</option>
+      <option value="0">不限</option>
+    </select>
+    <br />
+	<br />
+    <input type="radio" name="radioSet" id="option1" value="26" checked="checked" />
+    <label for="option1">蒙牛</label>
+    <br />
+    <input type="radio" name="radioSet" id="option2" value="28" />
+    <label for="option2">伊利</label>
+    <br />
+    <input type="radio" name="radioSet" id="option3" value="29" />
+    <label for="option3">光明</label>
+    <br />
+ 
+     
+    <br />
+    <label for="textinput">Min(Price):</label>
+    <br />
+    <input type="text" id="textinput" name="textinput" size="15" />
+    <br />
+    <label for="passwordinput">Max(Price):</label>
+    <br />
+    <input type="text" id="textinput" name="textinput2" size="15" />
+    <br />
+    
+    <br />
+    <input type="submit" class="button-primary" value="OK!" />
+  </form>
+  
+</div>
+
+<div style="width:550px;margin:20px auto;">
+<p><strong><a href="http://js.mobanwang.com"></a></strong> </p>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function(){
+	$.NiceJForms.build({
+			imagesPath:"css-com/images/greentheme/"
+		})
+	});
+</script>
+				  	
+                 <!--end-->					
 				</div>
 			
 			</div>
